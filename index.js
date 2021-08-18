76ac0f38601f8fe4bfceb08b562f7bebf2f15098
npstrass <npstrass@outlook.com>
2021-08-18 15:13:43 -0500

    some java

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-const rules = document.getElementById('rules')
-const closeBtn = document.getElementById('close')
-const modal = document.querySelector(".modal-screen")
+const rules = document.querySelector("#rules");
+const closeBtn = document.querySelector("#close");
+const modal = document.querySelector(".modal-screen");
 
-rules.addEventListener('click', () => {
-    modal.classList.remove('display-none')
-})
+const score = document.querySelector("#score");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const triangle = document.querySelector(".triangle");
 
-closeBtn.addEventListener('click', () => {
-    modal.classList.add('display-none')
-})
+rules.addEventListener("click", () => {
+  modal.classList.remove("display-none");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.add("display-none");
+});
+
+
+
+
+
+
+
+
+
+
+let computerChoice = Math.floor(Math.random() * 3) + 1;
+if (computerChoice === 1) {
+  computerChoice = "rock";
+} else if (computerChoice === 2) {
+  computerChoice = "paper";
+} else if (computerChoice === 3) {
+  computerChoice = "scissors";
+}
+
+console.log(computerChoice)
+
+rock.addEventListener("click", () => {
+  userChoice = 'rock';
+});
+
+paper.addEventListener("click", () => {
+  userChoice = 'paper';
+});
+
+scissors.addEventListener("click", () => {
+  userChoice = 'scissors';
+});
