742c11e609ef70f5393fade8d17cfe95ede22087
npstrass <npstrass@outlook.com>
2021-08-19 11:43:22 -0500

    changes

--- index.js
+++ index.js
@@ -6,40 +6,57 @@ const compChoice = document.getElementById("compChoice");
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
+const gameOver = document.getElementById('gameOver')
+const playAgain = document.getElementById('playAgain')
 
 function getCompChoice() {
   let compSelect = Math.floor(Math.random() * 3) + 1;
 
   if (compSelect === 1) {
     compSelect = "rock";
-    compChoice.classList.add("rock");
+    setTimeout(() => {
+      compChoice.classList.add("rock");
+    }, 1000);
   } else if (compSelect === 2) {
     compSelect = "paper";
-    compChoice.classList.add("paper");
+    setTimeout(() => {
+      compChoice.classList.add("paper");
+    }, 1000);
   } else if (compSelect === 3) {
     compSelect = "scissors";
-    compChoice.classList.add("scissors");
+    setTimeout(() => {
+      compChoice.classList.add("scissors");
+    }, 2000);
   }
+  return compSelect;
 }
-
-rock.addEventListener("click", () => {
-  userSelect = "rock";
-  runGame("rock");
-});
-
-paper.addEventListener("click", () => {
-  userSelect = "paper";
-  runGame("paper");
-});
-
-scissors.addEventListener("click", () => {
-  userSelect = "scissors";
-  runGame("scissors");
-});
 
 function runGame(pick) {
   gameContainer.classList.add("disNone");
   playContainer.classList.remove("disNone");
   userChoice.classList.add(`${pick}`);
-  getCompChoice();
+  let x = getCompChoice();
+  return x;
 }
+
+rock.addEventListener("click", () => {
+  let y = runGame("rock");
+  function declareWinner() {
+    if (y === "rock") {
+      console.log("its a tie");
+    } else if (y === "paper") {
+      console.log("you lost");
+    } else if (y === "scissors") {
+      console.log("you won");
+    }
+  }
+  setTimeout(declareWinner, 2500);
+});
+
+paper.addEventListener("click", () => {
+  runGame("paper");
+});
+
+scissors.addEventListener("click", () => {
+  runGame("scissors");
+});
