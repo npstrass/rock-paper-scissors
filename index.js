a5e550f9aa7f0ce97fd52f19ee088d10e9a8bb1a
npstrass <npstrass@outlook.com>
2021-08-19 10:32:36 -0500

    updates

--- index.js
+++ index.js
@@ -6,34 +6,40 @@ const compChoice = document.getElementById("compChoice");
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
-const compSelect = getCompChoice();
 
 function getCompChoice() {
-  let compChoice = Math.floor(Math.random() * 3) + 1;
+  let compSelect = Math.floor(Math.random() * 3) + 1;
 
-  if (compChoice === 1) {
-    compChoice = "rock";
-  } else if (compChoice === 2) {
-    compChoice = "paper";
-  } else if (compChoice === 3) {
-    compChoice = "scissors";
+  if (compSelect === 1) {
+    compSelect = "rock";
+    compChoice.classList.add("rock");
+  } else if (compSelect === 2) {
+    compSelect = "paper";
+    compChoice.classList.add("paper");
+  } else if (compSelect === 3) {
+    compSelect = "scissors";
+    compChoice.classList.add("scissors");
   }
-  return compChoice;
 }
-
-console.log(compSelect);
 
 rock.addEventListener("click", () => {
   userSelect = "rock";
-  console.log("rock");
+  runGame("rock");
 });
 
 paper.addEventListener("click", () => {
   userSelect = "paper";
-  console.log("paper");
+  runGame("paper");
 });
 
 scissors.addEventListener("click", () => {
   userSelect = "scissors";
-  console.log("scissors");
+  runGame("scissors");
 });
+
+function runGame(pick) {
+  gameContainer.classList.add("disNone");
+  playContainer.classList.remove("disNone");
+  userChoice.classList.add(`${pick}`);
+  getCompChoice();
+}
