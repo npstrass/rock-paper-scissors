2695ab5de0285b69c0e2c36251b131a2c9bd7fea
npstrass <npstrass@outlook.com>
2021-08-19 21:35:39 -0500

    style

--- index.js
+++ index.js
@@ -1,91 +1,129 @@
+const gameOverContainer = document.getElementById("gameOverContainer");
 const gameContainer = document.getElementById("gameContainer");
 const playContainer = document.getElementById("playContainer");
-const score = document.getElementById("score");
-const userChoice = document.getElementById("userChoice");
-const compChoice = document.getElementById("compChoice");
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
-const gameOverContainer = document.getElementById("gameOverContainer");
-const gameOver = document.getElementById("gameOver");
+const userPick = document.getElementById("userChoice");
+const compPick = document.getElementById("compChoice");
 const playAgain = document.getElementById("playAgain");
-score.innerText = 0;
+const gameOver = document.getElementById("gameOver");
+const scissors = document.getElementById("scissors");
+const paper = document.getElementById("paper");
+const rock = document.getElementById("rock");
+const score = document.getElementById("score");
+let scoreValue = 0;
+score.innerText = scoreValue;
 
-function getCompChoice() {
-  let compSelect = Math.floor(Math.random() * 3) + 1;
-
-  if (compSelect === 1) {
-    compSelect = "rock";
-    setTimeout(() => {
-      compChoice.classList.add("rock");
-    }, 1000);
-  } else if (compSelect === 2) {
-    compSelect = "paper";
-    setTimeout(() => {
-      compChoice.classList.add("paper");
-    }, 1000);
-  } else if (compSelect === 3) {
-    compSelect = "scissors";
-    setTimeout(() => {
-      compChoice.classList.add("scissors");
-    }, 2000);
+function runGame(userChoice) {
+  function getRandNum() {
+    return Math.floor(Math.random() * 3) + 1;
   }
-  return compSelect;
-}
 
-function runGame(pick) {
-  gameContainer.classList.add("disNone");
-  playContainer.classList.remove("disNone");
-  userChoice.classList.add(`${pick}`);
-  let x = getCompChoice();
-  return x;
-}
+  const randNum = getRandNum();
 
-rock.addEventListener("click", () => {
-  let y = runGame("rock");
-  function declareWinner() {
-    gameOverContainer.classList.remove("disNone");
-    if (y === "rock") {
-      gameOver.innerText = "you tied";
-    } else if (y === "paper") {
-      gameOver.innerText = "you lost";
+  function getCompChoice() {
+    if (randNum === 1) {
+      setTimeout(() => {
+        compPick.classList.add("rock");
+      }, 1000);
+      return "rock";
+    } else if (randNum === 2) {
+      setTimeout(() => {
+        compPick.classList.add("paper");
+      }, 1000);
+      return "paper";
+    } else if (randNum === 3) {
+      setTimeout(() => {
+        compPick.classList.add("scissors");
+      }, 1000);
+      return "scissors";
+    }
+  }
+
+  const compChoice = getCompChoice();
+
+  if (userChoice === compChoice) {
+    gameOver.innerText = "you tied";
+  }
+
+  if (userChoice === "rock") {
+    userPick.classList.add("rock");
+    if (compChoice === "scissors") {
+      gameOver.innerText = "you win";
+    } else if (compChoice === "paper") {
+      gameOver.innerText = "you lose";
+    }
+  } else if (userChoice === "paper") {
+    userPick.classList.add("paper");
+    if (compChoice === "rock") {
+      gameOver.innerText = "you win";
+    } else if (compChoice === "scissors") {
+      gameOver.innerText = "you lose";
+    }
+  } else if (userChoice === "scissors") {
+    userPick.classList.add("scissors");
+    if (compChoice === "paper") {
+      gameOver.innerText = "you win";
+    } else if (compChoice === "rock") {
+      gameOver.innerText = "you lose";
+    }
+  }
+
+  if (gameOver.innerText === "you win") {
+    setTimeout(() => {
+      document.getElementById("userDidWin").classList.remove("disNone");
+      score.innerText = parseInt(score.innerText) + 1;
+    }, 1000);
+  } else if (gameOver.innerText === "you lose") {
+    setTimeout(() => {
+      document.getElementById("compDidWin").classList.remove("disNone");
       if (parseInt(score.innerText) > 0) {
-          score.innerText = parseInt(score.innerText) - 1
+        score.innerText = parseInt(score.innerText) - 1;
       }
-    } else if (y === "scissors") {
-      gameOver.innerText = "you won";
-      score.innerText = parseInt(score.innerText) + 1
-    }
+    }, 1000);
   }
-  setTimeout(declareWinner, 2500);
+}
+
+document.getElementById("rock").addEventListener("click", () => {
+  playContainer.classList.remove("disNone");
+  gameContainer.classList.add("disNone");
+  setTimeout(() => {
+    gameOverContainer.classList.remove("disNone");
+  }, 1000);
+  runGame("rock");
 });
 
-paper.addEventListener("click", () => {
-  let y = runGame("paper");
-  function declareWinner() {
-    if (y === "paper") {
-      console.log("its a tie");
-    } else if (y === "scissors") {
-      console.log("you lost");
-    } else if (y === "rock") {
-      console.log("you won");
-    }
-  }
-  setTimeout(declareWinner, 2500);
+document.getElementById("paper").addEventListener("click", () => {
+  playContainer.classList.remove("disNone");
+  gameContainer.classList.add("disNone");
+  setTimeout(() => {
+    gameOverContainer.classList.remove("disNone");
+  }, 1000);
+  runGame("paper");
 });
 
-scissors.addEventListener("click", () => {
-  let y = runGame("scissors");
-  function declareWinner() {
-    if (y === "scissors") {
-      console.log("its a tie");
-    } else if (y === "rock") {
-      console.log("you lost");
-    } else if (y === "paper") {
-      console.log("you won");
-    }
-  }
-  setTimeout(declareWinner, 2500);
+document.getElementById("scissors").addEventListener("click", () => {
+  playContainer.classList.remove("disNone");
+  gameContainer.classList.add("disNone");
+  setTimeout(() => {
+    gameOverContainer.classList.remove("disNone");
+  }, 1000);
+  runGame("scissors");
 });
 
-playAgain.addEventListener('click', {runGame})
+playAgain.addEventListener("click", () => {
+  playContainer.classList.add("disNone");
+  gameContainer.classList.remove("disNone");
+  gameOverContainer.classList.add("disNone");
+  document.getElementById("userDidWin").classList.add("disNone");
+  document.getElementById("compDidWin").classList.add("disNone");
+
+  userPick.className = "userChoice";
+  compPick.className = "compChoice";
+});
+
+document.getElementById('rules').addEventListener('click', () => {
+  document.getElementById('modal').classList.remove('disNone')
+})
+
+document.getElementById('close').addEventListener('click', () => {
+  document.getElementById('modal').classList.add('disNone')
+})
